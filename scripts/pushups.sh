855025c096fac156bedd4af21bc624f95e7963aa
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-07 18:38:06 -0500

    wip

---
+++ scripts/pushups.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+exec < /dev/tty
+
+echo "Before committing, please do 10 pushups! Press Enter when you're done:"
+
+# Wait for the user to press Enter
+read
+
+# Exit with success
+exit 0
