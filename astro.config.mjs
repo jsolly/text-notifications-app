58669533acce540baf931e6620507e881fa4d834
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-09 14:21:50 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	site: "https://www.johnsolly.dev",
+	integrations: [
+		tailwind(),
+		sitemap({
+			customPages: ["https://www.johnsolly.dev/John-Solly-Resume.pdf"],
+		}),
+	],
 });
