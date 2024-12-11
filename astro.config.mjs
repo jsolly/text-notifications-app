3dc70bd835ebe00cdac589b6c06e214b03eeff6a
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-11 17:34:54 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.johnsolly.dev",
-	integrations: [
-		tailwind(),
-		sitemap({
-			customPages: ["https://www.johnsolly.dev/John-Solly-Resume.pdf"],
-		}),
-	],
+	site: "https://www.text-notifications.app",
+	integrations: [tailwind(), sitemap({}), alpinejs()],
 });
