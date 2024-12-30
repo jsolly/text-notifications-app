99470fa4c986d7f96d98d4be0c4496f140a942bc
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-29 21:28:47 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.text-notifications.app",
-	integrations: [tailwind(), sitemap({}), alpinejs()],
+	integrations: [tailwind(), sitemap({}), alpine()],
 });
