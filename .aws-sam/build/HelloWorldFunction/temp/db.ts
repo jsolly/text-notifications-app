e66e45776c6332779979188a9ab5077718d670f0
John Solly <9572232+jsolly@users.noreply.github.com>
2025-02-09 17:48:43 -0500

    feat_user_sign_up_final

---
+++ .aws-sam/build/HelloWorldFunction/temp/db.ts
@@ -0,0 +1,9 @@
+// import { Client } from "pg";
+
+// export const getDbClient = async (): Promise<Client> => {
+// 	const client = new Client({
+// 		connectionString: process.env.DATABASE_URL,
+// 	});
+// 	await client.connect();
+// 	return client;
+// };
