18ff195028cedfea82489068ec70b1a227570362
John Solly <9572232+jsolly@users.noreply.github.com>
2025-02-09 15:06:04 -0500

    .

--- lambda/notifications/signup-processor/temp/db.ts
+++ lambda/notifications/signup-processor/temp/db.ts
@@ -1,9 +1,9 @@
-import { Client } from "pg";
+// import { Client } from "pg";
 
-export const getDbClient = async (): Promise<Client> => {
-	const client = new Client({
-		connectionString: process.env.DATABASE_URL,
-	});
-	await client.connect();
-	return client;
-};
+// export const getDbClient = async (): Promise<Client> => {
+// 	const client = new Client({
+// 		connectionString: process.env.DATABASE_URL,
+// 	});
+// 	await client.connect();
+// 	return client;
+// };
--- test.ts
+++
@@ -1,7 +0,0 @@
-await client.execute({
-	name: "insert-user",
-	text: `
-    INSERT INTO users (${Object.keys(FORM_FIELDS).join(", ")})
-    VALUES (${Object.values(formData).join(", ")})
-  `,
-});
