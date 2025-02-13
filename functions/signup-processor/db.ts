8b2d29ba659c7f43590b1d5e726052bc90077a65
John Solly <9572232+jsolly@users.noreply.github.com>
2025-02-12 19:56:45 -0500

    .

--- functions/signup-processor/db.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "pg";
-
-export const getDbClient = async (): Promise<Client> => {
-	const client = new Client({
-		connectionString: process.env.DATABASE_URL,
-	});
-	await client.connect();
-	return client;
-};
