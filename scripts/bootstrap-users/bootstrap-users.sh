713fff73cf0a46c8464d1caa3a73a23f33126512
John Solly <9572232+jsolly@users.noreply.github.com>
2025-03-09 21:27:53 -0400

    .

--- scripts/bootstrap-users/bootstrap-users.sh
+++ scripts/bootstrap-users/bootstrap-users.sh
@@ -9,8 +9,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Load environment variables
 if [ -f .env ]; then
     source .env
-elif [ -f "$SCRIPT_DIR/../.env" ]; then
-    source "$SCRIPT_DIR/../.env"
+elif [ -f "$SCRIPT_DIR/../../.env" ]; then
+    source "$SCRIPT_DIR/../../.env"
 fi
 
 # Check if database URL is provided
